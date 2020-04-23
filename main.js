61c34f87ff5546ef3010042197edaaf7bd639ade
MMAGANADEBIA <mmaganadebia@localhost.localdomain>
2020-04-22 20:10:14 -0500

    boton para push funcionando

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let btn = document.querySelector("#btn").addEventListener("click", data);
+let pushButton = document.querySelector("#pushButton").addEventListener("click", data);
 
 var pila = [];
 
@@ -6,7 +6,24 @@ function data(){ let numero = document.querySelector("#numero").value; add(numer
 
 function add(numero){
   pila.push(numero);
+  //console.log(pila);
+  impresion(pila);
+}
+
+function impresion(pila){
   console.log(pila);
+  let impresionPila = document.querySelector("#impresionPila");
+  impresionPila.innerHTML = "pila = " + pila;
+  //return pila;
+}
+
+let popButton = document.querySelector("#popButton").addEventListener("click", popPila);
+
+function popPila(){
+  let popPila = document.querySelector("#popPila");
+  pila.pop(); //pila.pop is not a function
+  popPila.innerHTML = "Fila despues de aplicarle pop = " + pila;
+  //console.log(pila)
 }
 
 /*var btn = document.querySelector("#btn");  Primera version, duplicaba numeros
