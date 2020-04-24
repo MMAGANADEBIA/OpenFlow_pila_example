63c03d4083428fe48451a397668b4623830cb9be
MMAGANADEBIA <42789411+MMAGANADEBIA@users.noreply.github.com>
2020-04-24 03:19:14 +0000

    segunda refactorización

--- main.js
+++ main.js
@@ -1,34 +1,22 @@
 var pila = [];
 
-let data = () => { let numero = document.querySelector("#numero").value; add(numero); } //Push
-let add = (numero) => { pila.push(numero); impresion(pila); }
-let impresion = (pila) => { let impresionPila = document.querySelector("#impresionPila"); impresionPila.innerHTML = "pila = " + pila; } //push
+let pushButton = document.querySelector("#pushButton").addEventListener("click", () => { let numero = document.querySelector("#numero").value; pila.push(numero);
+impresionPila=document.querySelector("#impresionPila"); impresionPila.innerHTML = "pila = " + pila;}); //Push
 
-let data2 = () => { let unshiftNumber = document.querySelector("#unshiftNumber").value; impresionUnshift(unshiftNumber); } //unshift
-impresionUnshift = (unshiftNumber) =>{ pila.unshift(unshiftNumber); let unshiftPila = document.querySelector("#unshiftPila");
-  unshiftPila.innerHTML= "Pila despues de aplicarle unshift = " + pila; } //unshift
+let unshiftButton = document.querySelector("#unshiftButton").addEventListener("click", () => {let unshiftNumber = document.querySelector("#unshiftNumber").value; pila.unshift(unshiftNumber);
+let unshiftPila = document.querySelector("#unshiftPila"); unshiftPila.innerHTML= "Pila despues de aplicarle unshift = " + pila;}); //unshift
 
-let popPila = () => {let popPila = document.querySelector("#popPila"); pila.pop(); //pop
-    popPila.innerHTML = "Fila despues de aplicarle pop = " + pila;} //pop
+let popButton = document.querySelector("#popButton").addEventListener("click", () => {let popPila = document.querySelector("#popPila"); pila.pop();
+popPila.innerHTML = "Fila despues de aplicarle pop = " + pila;}); //pop
 
-let shiftPila = () => { let shiftPila = document.querySelector("#shiftPila"); pila.shift(); //shift
-      shiftPila.innerHTML = "Pila despues de aplicarle shift = " + pila;}//shift
+let shiftButton = document.querySelector("#shiftButton").addEventListener("click", () => {let shiftPila = document.querySelector("#shiftPila"); pila.shift();
+shiftPila.innerHTML = "Pila despues de aplicarle shift = " + pila;}); //shift
 
-let sortPila = () => { let sortPila = document.querySelector("#sortPila"); pila.sort(compareFunction); //sort
-      sortPila.innerHTML="Pila de menor a mayor = " + pila;}//sort
-      compareFunction = (a, b) => { return a - b; }//funcion de comparacion para numeros sort
+let sortButton = document.querySelector("#sortButton").addEventListener("click", () => {let sortPila = document.querySelector("#sortPila");let compareFunction = (a, b) => { return a - b; }; pila.sort(compareFunction);
+sortPila.innerHTML="Pila de menor a mayor = " + pila;}); //sort
 
-let sortPila2 = () => {let sortPila = document.querySelector("#sortPila"); pila.sort(compareFunction2); //sort inverso
-      sortPila.innerHTML = "Pila de mayor a menor = " + pila;} //sort inverso
-      compareFunction2 = (a, b) => {return b - a;} //funcion de comparacion sort inverso para numeros
+let sortIn = document.querySelector("#sortIn").addEventListener("click", () => {let sortPila = document.querySelector("#sortPila");let compareFunction2 = (a, b) => {return b - a;}; pila.sort(compareFunction2); //sort inverso
+sortPila.innerHTML = "Pila de mayor a menor = " + pila;}); //sort inverso
 
-let reversePila = () => {let reversePila = document.querySelector("#reversePila"); pila.reverse(); //reverse
-        reversePila.innerHTML = "Pila despues de aplicarle reverse = " + pila;} //reverse
-
-let pushButton = document.querySelector("#pushButton").addEventListener("click", data); //Bloque de declaracion de botones
-let unshiftButton = document.querySelector("#unshiftButton").addEventListener("click", data2);
-let popButton = document.querySelector("#popButton").addEventListener("click", popPila);
-let shiftButton = document.querySelector("#shiftButton").addEventListener("click", shiftPila);
-let sortButton = document.querySelector("#sortButton").addEventListener("click", sortPila);
-let sortIn = document.querySelector("#sortIn").addEventListener("click", sortPila2);
-let reverseButton = document.querySelector("#reverseButton").addEventListener("click", reversePila); //bloque de declaracion de variables
+let reverseButton = document.querySelector("#reverseButton").addEventListener("click", () => {let reversePila = document.querySelector("#reversePila"); pila.reverse(); //reverse
+reversePila.innerHTML = "Pila despues de aplicarle reverse = " + pila;}); //reverse
